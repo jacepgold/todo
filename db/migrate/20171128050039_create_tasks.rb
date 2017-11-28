6cb2928879db0d19f139763b4fd7f5c8a66f45c0
Jace P. Gold <Jace@JaceGold.com>
2017-11-27 23:25:11 -0700

    Clean up

--- db/migrate/20171128050039_create_tasks.rb
+++ db/migrate/20171128050039_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
       t.boolean :completed, default: false
 
       t.timestamps
