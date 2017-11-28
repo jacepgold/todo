bdbb1c0cc97385224ffc632f328331afaf7802ba
Jace P. Gold <Jace@JaceGold.com>
2017-11-28 00:03:23 -0700

    Fix bugs and add style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,16 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
   
   def update
     @task = Task.find(params[:id])
@@ -29,6 +39,8 @@ class TasksController < ApplicationController
     Task.find(params[:id]).destroy
     redirect_to tasks_path
   end 
+
+
   private
   def task_params
     params.require(:task).permit(:title, :description, :completed)
