a8d408b48bdf16d3ea6d98a5c9aa7e76beb556ef
Jace P. Gold <Jace@JaceGold.com>
2017-11-27 23:45:23 -0700

    Finish Basic Objectives

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,17 +10,25 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-  
-  def create
-    @task = Task.new(task_params)
 
-    if @task.save
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
       redirect_to tasks_path
     else
-      render :new
+      render :edit
     end
   end
 
+  def destroy 
+    Task.find(params[:id]).destroy
+    redirect_to tasks_path
+  end 
   private
   def task_params
     params.require(:task).permit(:title, :description, :completed)
